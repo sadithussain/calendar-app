27d874a9ab17793ad50effbba7b677440778f65d
Sadit Hussain <wfrblx2244@gmail.com>
2026-01-15 22:05:26 -0500

    Finished landing, login, and registration pages

---
+++ calendarbackend/src/main/java/com/sadithussain/calendarapp/calendarbackend/CalendarbackendApplication.java
@@ -0,0 +1,13 @@
+package com.sadithussain.calendarapp.calendarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarbackendApplication.class, args);
+    }
+
+}
---
+++ calendarbackend/src/test/java/com/sadithussain/calendarapp/calendarbackend/CalendarbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadithussain.calendarapp.calendarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
